40cb94a15fcf0b3303d7fadb9990f46ffdfc1adf
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-11-05 00:06:12 -0500

    built out dice game

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -67,11 +67,38 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
+
+// calcAge(1985);
+
+// function calcAge(year) {
+//     console.log(2016 - year);
+//     console.log(this);
+// }
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calcAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+}
+
+john.calcAge();
 
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
 
+mike.calcAge = john.calcAge;
 
-
-
-
-
+mike.calcAge();
